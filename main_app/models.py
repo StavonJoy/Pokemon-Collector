4c448d9d35c20abb3d76a8c660fef6e1e1873442
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-06 16:02:02 -0400

    adds pokemon detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+  name = models.CharField(max_length=100)
+  type = models.CharField(max_length=100)
+  height = models.IntegerField()
+  weight = models.IntegerField()
+  description = models.TextField(max_length=250)
+  level = models.IntegerField()
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,19 @@
 from django.shortcuts import render
+from .models import Pokemon
 
 # Create your views here.
 from django.http import HttpResponse
 
-class Pokemon:
-  def __init__(self, name, type, height, weight, description, level):
-    self.name = name
-    self.type = type
-    self.height = height
-    self.weight = weight
-    self.description = description
-    self.level = level
-
-pokemon = [
-  Pokemon('Eevee', 'normal', '1 ft', '14.3 lbs', 'It has the ability to alter the composition of its body to suit its surrounding environment.', 15),
-  Pokemon('Vulpix', 'fire', '2 ft', '21.8 lbs', 'While young it has six gorgeous tails. When it grows, several new tails are sprouted.', 6),
-  Pokemon('Mareep', 'electric', '2 ft', '17.2 lbs', 'Clothing made from Mareep’s fleece is easily charged with static electricity, so a special process is used on it.', 10)
-]
-
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def pokemon_index(request):
+  pokemon = Pokemon.objects.all()
   return render(request, 'pokemon/index.html', {'pokemon': pokemon })
+
+def pokemon_detail(request, pokemon_id):
+  pokemon = Pokemon.objects.get(id=pokemon_id)
+  return render(request, 'pokemon/detail.html', { 'pokemon': pokemon })
