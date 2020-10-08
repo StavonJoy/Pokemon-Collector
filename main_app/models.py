478e7a78f284b9eaf5a67c5030b420e655308e93
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-10-08 10:16:58 -0400

    Adds berry feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.urls import reverse
+
+BERRIES = (
+  ('O', "Oran"),
+  ('C', "Chesto"),
+  ('P', 'Pecha'),
+  ('R', 'Rawst'),
+  ('A', 'Aspear'),
+  ('S', 'Sitrus')
+)
 
 # Create your models here.
 class Pokemon(models.Model):
@@ -11,3 +21,22 @@ class Pokemon(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'pokemon_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  berry = models.CharField(
+    max_length=1, 
+    choices=BERRIES, 
+    default=BERRIES[0][0]
+    )
+
+  pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_berry_display()} on {self.date}'
+  
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Pokemon
+from .forms import FeedingForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -16,4 +18,25 @@ def pokemon_index(request):
 
 def pokemon_detail(request, pokemon_id):
   pokemon = Pokemon.objects.get(id=pokemon_id)
-  return render(request, 'pokemon/detail.html', { 'pokemon': pokemon })
+  feeding_form = FeedingForm()
+  return render(request, 'pokemon/detail.html', { 'pokemon': pokemon, 'feeding_form': feeding_form })
+
+def feed_berry(request, pokemon_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.pokemon_id = pokemon_id
+    new_feeding.save()
+  return redirect('detail', pokemon_id=pokemon_id)
+
+class PokemonCreate(CreateView):
+  model = Pokemon
+  fields = '__all__'
+
+class PokemonUpdate(UpdateView):
+  model = Pokemon
+  fields = ['type', 'height', 'weight', 'description', 'level']
+
+class PokemonDelete(DeleteView):
+  model = Pokemon
+  success_url = '/pokemon/'
